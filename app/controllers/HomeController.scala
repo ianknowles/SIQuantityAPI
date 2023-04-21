348c75273c7b1360fb59e8beae8d1a71b6b3092e
ianknowles <ian@imknowles.co.uk>
2023-04-21 11:14:36 +0100

    add messages support to example controller

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import javax.inject._
 import play.api._
+import play.api.i18n.I18nSupport
 import play.api.mvc._
 
 /**
@@ -9,7 +10,7 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with I18nSupport {
 
   /**
    * Create an Action to render an HTML page.
