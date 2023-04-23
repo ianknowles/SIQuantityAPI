00edbb4eae0fa349dc6e6f6f838e1bbab6b9daf2
ianknowles <ian@imknowles.co.uk>
2023-04-23 15:11:53 +0100

    improve table in html view and internationalize the messages

--- app/models/SIUnit.scala
+++ app/models/SIUnit.scala
@@ -1,9 +1,15 @@
 package models
 
+import models.SIUnit.api_path
 import play.api.libs.json._
 
-case class SIUnit(symbol: String, name: String, name_fr: String)
+case class SIUnit(symbol: String, name: String, name_fr: String) {
+	val relativeIRI: String = s"/$api_path/$symbol"
+}
 
 object SIUnit {
-  implicit val SIUnitFormat: Format[SIUnit] = Json.format[SIUnit]
+	// TODO get api path from reverse router?
+	private val api_path: String = "units"
+
+	implicit val SIUnitFormat: Format[SIUnit] = Json.format[SIUnit]
 }
